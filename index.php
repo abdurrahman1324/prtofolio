552724ca673fce8d882f540d6cd66bde0280e787
abdurrahman1324 <n.a.a.a.i.djokam@gmail.com>
2024-09-02 19:13:34 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,181 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/edica/edica/assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="assets/edica/edica/assets/vendors/font-awesome/css/all.min.css">
+    <link href="assets/edica/edica/assets/vendors/aos/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/edica/edica/assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="assets/edica/edica/assets/vendors/jquery/jquery.min.js"></script>
+    <script src="assets/edica/edica/assets/js/loader.js"></script>
+</head>
+<body>
+<div class="edica-loader"></div>
+<header class="edica-header edica-landing-header sticky-navbar">
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="index.html"><span><b>Portofolio</b></span></a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#edicaMainNav" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="edicaMainNav">
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a id="home" class="nav-link" href="#index">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a id="#projectc" href="#projectc" class="nav-link">Projectc</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#certification">Certification</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+</header>
+        <section>
+            <div class="container">
+                <div class="profile">
+                <img data-aos="fade-down-left" class="rounded-circle profileImage"src="assets/image/profile.jpg" alt="">
+                <div class="profileText">
+                    <h2 data-aos="fade-up-right">Hallo <span class="wave-icon">👋</span> Saya Abdurrahman 
+                    </h2>
+                </div>
+                    <h4 data-aos="fade-up-left">Web Devlopement</h4>
+                </div>
+            </div>
+        </section>
+    </div>
+<main>
+        <section id="about"class="edica-landing-section edica-landing-about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6" data-aos="fade-up-right">
+                        <h4 class="edica-landing-section-subtitle-alt">ABOUT US</h4>
+                        <p>Hallo, Perkenalkan saya Abdurrahman. Saat ini saya masih menempuh pendidikan Sekolah Menengah Kejurusan/SMK Jurusan PPLG di SMKN 8 PEKANBARU. Kab. Riau jl. Tengku kasim perkasa</p>
+                        <ul class="landing-about-list">
+                            <li>Pendidikan</li>
+                            <p><b>2011,</b> TK Quantum Kids, Pekanabaru</p>
+                            <p><b>2013,</b> SD Negeri 162, Pekanabaru</p>
+                            <p><b>2019,</b> SMP Negeri 24, Pekanabaru</p>
+                            <p><b>2021,</b> SMK Negeri 8, Pekanabaru</p>
+                        </ul>
+                    </div>
+                    <div class="col-md-6" data-aos="fade-up-left">
+                        <img src="assets/image/profile.jpg" width="300px" alt="about" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="projectc" class="edica-landing-section edica-landing-clients">
+            <div class="container">
+                <h4 class="edica-landing-section-subtitle-alt">PROJECTC</h4>
+                <div class="partners-wrapper">
+                    <img src="assets/image/p.png" alt="client logo" data-aos="flip-right" data-aos-delay="250">
+                    <img src="assets/image/a.png" alt="client logo" data-aos="flip-right" data-aos-delay="500">
+                </div>
+            </div>
+        </section>
+        <section id="certification" class="edica-landing-section edica-landing-services">
+            <div class="container">
+            <h4  class="edica-landing-section-subtitle-alt">CERTIFICATION</h4>
+            <table class="table text-center" id="dataTables-example">
+                <thead>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Organizer</th>
+                    <th>Date</th>
+                    <th>Credetial</th>
+                </thead>
+                <tbody>
+                    <td>1</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td>-</td>
+                    <td><button class="btn btn-primary">View</button></td>
+                </tbody>
+               </table>
+                </div>
+            </div>
+        </section>
+        <section id="contact" class="edica-landing-section edica-landing-testimonials" data-aos="fade-up">
+            <div class="container">
+            <h4  class="edica-landing-section-subtitle-alt">CONTACTC</h4>
+                <div class="logo-container">
+                    <a href="https://www.instagram.com" target="_blank">
+                        <img src="assets/image/iconig.jpeg" class="logo" alt="Instagram">
+                        Instagaram
+                    </a>
+                    <a href="https://www.facebook.com" target="_blank">
+                        <img src="assets/image/fb.png" class="logo" alt="Facebook">
+                        Facebook
+                    </a>
+                    <a href="https://www.youtube.com" target="_blank">
+                        <img src="assets/image/logoyt.png" class="logo" alt="YouTube">
+                        YouTube
+                    </a>
+                    <a href="https://twitter.com" target="_blank">
+                        <img src="assets/image/twiter.png" class="logo" alt="Twitter">
+                        Twitter 
+                    </a>
+                </div>
+            </div>
+        </section>
+    <footer  class="edica-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <p class="contact-details">hello@Portofolio.com</p>
+                    <p class="contact-details">+23 3000 000 00</p>
+                    <nav class="footer-social-links">
+                        <a href="https://www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+                    </nav>
+                </div>
+                <div class="col-md-5">
+                    <nav class="footer-nav">
+                        <a href="#!" class="nav-link">Company</a>
+                        <a href="#!" class="nav-link">Android App</a>
+                        <a href="#!" class="nav-link">ios App</a>
+                        <a href="#!" class="nav-link">Blog</a>
+                        <a href="#!" class="nav-link">Partners</a>
+                        <a href="#!" class="nav-link">Careers</a>
+                    </nav>
+                </div>
+              
+            </div>
+            <div class="footer-bottom-content">
+                <nav class="nav footer-bottom-nav">
+                    <a href="#!">Privacy & Policy</a>
+                    <a href="#!">Terms</a>
+                    <a href="#!">Site Map</a>
+                </nav>
+                <p class="mb-0">© Abdurrahman. 2020 <a href="https://www.bootstrapdash.com" target="_blank" rel="noopener noreferrer" class="text-reset">bootstrapdash</a> . All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/edica/edica/assets/vendors/popper.js/popper.min.js"></script>
+    <script src="assets/edica/edica/assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/edica/edica/assets/vendors/aos/aos.js"></script>
+    <script src="assets/edica/edica/assets/js/main.js"></script>
+    <script>
+        AOS.init({
+            duration: 2000
+        });
+      </script>
+</body>
+</html>
